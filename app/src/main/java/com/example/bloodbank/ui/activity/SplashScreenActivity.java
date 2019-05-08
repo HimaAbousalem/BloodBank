188b9e31a99e23fdd1d300d6535c02736260a61c
HimaAbousalem <hima.abousalem@gmail.com>
2019-05-08 04:56:28 +0200

    finish splash screen

--- app/src/main/java/com/example/bloodbank/ui/activity/SplashScreenActivity.java
+++ app/src/main/java/com/example/bloodbank/ui/activity/SplashScreenActivity.java
@@ -1,14 +1,18 @@
-package com.example.bloodbank;
+package com.example.bloodbank.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.bloodbank.ui.activity.AuthenticationActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        startActivity(new Intent(this, WelcomeActivity.class));
+        finish();
     }
 }
